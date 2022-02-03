287fea5e25e19c206385192ddfa46d670f7821ee
azizzakiryarov <azizzakiryarov87@gmail.com>
2022-02-03 17:33:32 +0100

    Created scaffolding for project

---
+++ src/main/java/se/azza/recursivelymachine/main/Main.java
@@ -0,0 +1,7 @@
+package se.azza.recursivelymachine.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Recursively Machine is started!");
+    }
+}
